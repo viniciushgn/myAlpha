6c4d479486009d5067cc308c8d7d4b44c6c18285
viniciushgn <62067975+viniciushgn@users.noreply.github.com>
2023-03-16 22:54:15 -0300

    working backup

--- main.py
+++ main.py
@@ -44,3 +44,30 @@ st.write("""
 
 st.subheader('Raw data')
 st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='Stock Open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='Stock Close'))
+    fig.layout.update(title_text = "Time Series Data", xaxis_rangeslider_visible = True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
+
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close":"y"})
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader('Forecast data')
+st.write(forecast.tail())  
+
+st.write('forecast data')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write('forecast components')
+fig2 = m.plot_components(forecast)
+st.write(fig2)
